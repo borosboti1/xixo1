26448989fa91d65c89dd49800986e74ed60c7507
borosboti1 <148892133+borosboti1@users.noreply.github.com>
2025-03-02 15:57:33 +0100

    2025.03.02 15:57

--- script.js
+++ script.js
@@ -39,3 +39,40 @@ function closeMenu(event) {
 
 document.addEventListener('click', closeMenu);
 document.addEventListener('touchstart', closeMenu);
+
+window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY > 100;
+    navbar.style.background = scrolled 
+      ? 'linear-gradient(90deg, #000000ee, #1a1a1aee)' 
+      : 'linear-gradient(90deg, #00000080, #1a1a1a80)';
+    navbar.style.padding = scrolled ? '8px 0' : '12px 0';
+  });
+
+  window.addEventListener('scroll', function() {
+    const background = document.querySelector('.HeaderBackground');
+    if (window.scrollY > 100) {
+        background.style.opacity = '0';
+    } else {
+        background.style.opacity = '1';
+    }
+});
+
+
+
+
+document.querySelector('.lefele_nyil').addEventListener('click', () => {
+    document.getElementById('section1').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('section.teljeskep');
+    const scrollY = window.scrollY;
+
+    if (scrollY > header.clientHeight / 2) {
+        document.body.classList.add('scrolled');
+    } else {
+        document.body.classList.remove('scrolled');
+    }
+});
